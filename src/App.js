433d3b96e77d8ff5f1a997acdf7064c4e060b35e
camisayu <camilasayuri111@gmail.com>
2024-09-12 13:01:22 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Atualize para Routes
-import Home from './Telas/home';
+import Home from './Telas/Home';
 import InformacoesPet from './Telas/InformacoesPet';
 import './App.css';
 
