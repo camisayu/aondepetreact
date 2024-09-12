7f3e96927a4d2fd28c0dc47cf4f6d5c6a32794fa
camisayu <camilasayuri111@gmail.com>
2024-09-12 17:31:16 -0300

    Atualização

--- src/Telas/InformacoesPet.js
+++ src/Telas/InformacoesPet.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './InformacoesPet.css';
+import Carrossel from '../Componentes/Carrossel';
 
 function InformacoesPet() {
     const navigate = useNavigate();
@@ -8,13 +9,21 @@ function InformacoesPet() {
     const ToggleMode = () => {
       setMode(!active);
     }
+
+    const imagens = [
+      'https://images.unsplash.com/photo-1526526431900-88eb525f1e4a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1501820488136-72669149e0d4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://plus.unsplash.com/premium_photo-1664371206019-a82ba8d7c2e2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      // Adicione mais URLs de imagem conforme necessário
+    ];
+
     return (
       <div className="informacoes-pet">
         <div className="header">
   
-        <button className="back-button" onClick={() => navigate("/")}>
-          <i className="fas fa-chevron-left"></i> {/* Ícone de meia seta */}
-        </button>
+          <button className="back-button" onClick={() => navigate("/")}>
+            <i className="fas fa-chevron-left"></i> {/* Ícone de meia seta */}
+          </button>
   
           <button className="hamburger1" onClick={ToggleMode}>
             &#9776;
@@ -28,6 +37,16 @@ function InformacoesPet() {
             <li><Link to="/informacoes-pet">Informações do Pet</Link></li>
           </ul>
         </nav>
+
+        <div className="title1">
+          <h1>Bobby</h1>
+          <h2>Status: Perdido</h2>
+        </div>
+
+        <div className="carrossel-section">
+        <Carrossel imagens={imagens} />
+      </div>
+
       </div>
     );
   }
