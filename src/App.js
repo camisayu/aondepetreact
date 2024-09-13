ac412d0c13459e031a276600de1cc3ff49eaa735
camisayu <camilasayuri111@gmail.com>
2024-09-13 03:41:22 -0300

    Inicio da Tela Conta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Atualize para Routes
 import Home from './Telas/Home';
 import InformacoesPet from './Telas/InformacoesPet';
+import Conta from './Telas/Conta';
 import './App.css';
 
 function App() {
--- src/Telas/Conta.js
+++ src/Telas/Conta.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Conta.css'; // Importe o arquivo CSS se estiver usando um
+import './Conta.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
 
 const Conta = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="header">
-      <button className="back-button" onClick={() => navigate("/")}>
-        <i className="fas fa-chevron-left"></i> {/* Ícone de meia seta */}
-      </button>
-      {/* Você pode adicionar outros elementos ou conteúdo aqui */}
+    <div className= "Conta">
+        <div className="header">
+            <button className="back-button" onClick={() => navigate("/")}>
+                <i className="fas fa-chevron-left"></i> {/* Ícone de meia seta */}
+            </button>
+        
+            <div className="title2">
+                <h2>Sua Conta</h2>
+            </div>
+
+        </div>
+
+        <div className="icon-container">
+            <FontAwesomeIcon icon={faPaw} className="paw-icon" />
+        </div>
+
+        <div className="form-container">
+            <form className="login-form">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" placeholder="Digite seu email" />
+            
+                <label htmlFor="password">Senha:</label>
+                <input type="password" id="password" placeholder="Digite sua senha" />
+            
+                <button type="submit">Entrar</button>
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
     </div>
   );
 };
